528a327519baf7315cbeee609670641701a84ccf
Rayan-Ey <rayanahmed1805@gmail.com>
2024-05-13 23:19:06 +0530

    Added message and conversation models

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,7 +2,7 @@ import { mongoose } from "mongoose";
 
 const connectDB=async ()=>{
     try{
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log("connected to db");
     }catch(err){
         console.log("Internal server error/cant connect to db",err.message);
